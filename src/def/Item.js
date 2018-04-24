e5076deeff65ca1b0bf9dd57e2197615c1304042
Jaegar Sarauer <a00925935>
2018-04-24 01:16:29 -0700

    Added icons to items. Inventory and shop screens show your items better formatted & using icons

--- src/def/Item.js
+++ src/def/Item.js
@@ -1,37 +1,46 @@
+import * as IMAGE from './Image';
+
 export const ITEM = {
     Coins: {
         id: 0,
         name: 'Coins',
         value: 1,
+        icon: IMAGE.ITEMS.Coins,
     },
     Logs: {
         id: 1,
         name: 'Logs',
-        value: 3
+        value: 3,
+        icon: IMAGE.ITEMS.Logs,
     },
     OakLogs: {
         id: 2,
         name: 'Oak Logs',
-        value: 6
+        value: 6,
+        icon: IMAGE.ITEMS.Logs,
     },
     WillowLogs: {
         id: 3,
         name: 'Willow Logs',
-        value: 10
+        value: 10,
+        icon: IMAGE.ITEMS.Logs,
     },
     MapleLogs: {
         id: 4,
         name: 'Maple Logs',
-        value: 15
+        value: 15,
+        icon: IMAGE.ITEMS.Logs,
     },
     YewLogs: {
         id: 5,
         name: 'Yew Logs',
-        value: 20
+        value: 20,
+        icon: IMAGE.ITEMS.Logs,
     },
     ArbutusLogs: {
         id: 6,
         name: 'Arbutus Logs',
-        value: 30
+        value: 30,
+        icon: IMAGE.ITEMS.Logs,
     }
 };
--- src/ui/Shop.js
+++ src/ui/Shop.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View, Text, StyleSheet, FlatList, Modal} from 'react-native';
+import { Button, View, Text, StyleSheet, Modal, Image} from 'react-native';
 import {Player, PP} from '../state/Player'; 
 import { ITEM } from '../def/Item';
 
@@ -7,7 +7,6 @@ export default class Shop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      updateList: false,
       showSellModal: false,
       modalItem: null,
     };
@@ -22,43 +21,41 @@ export default class Shop extends React.Component {
     PP.removeItem(item, amount);
     PP.addItem(ITEM.Coins, amount * item.value);
     this.setState({
-      updateList: !this.state.updateList,
       showSellModal: false,
     });
   }
 
   render() {
+    const items = PP.items.filter(item => {return item.id !== ITEM.Coins.id});
+    if (items.length === 0)
+      return (<Text style={styles.centerText}>No items to sell!</Text>);
     return (
       <View style={styles.container}>
-        <FlatList
-          data={PP.items.filter(item => {return item.id !== ITEM.Coins.id})}
-          extraData={this.state.updateList}
-          keyExtractor={(item, index) => item.id.toString()}
-          renderItem={({item}) => 
-          <View onTouchEnd={() => {this.setState({showSellModal: true, modalItem: item})}}>
-            <Text 
-              style={styles.item}
-            >
-              {item.name + ": " + item.amount}
-            </Text>
+        {items.map(item =>
+          <View 
+            onTouchEnd={() => {this.setState({showSellModal: true, modalItem: item})}} 
+            key={item.id} 
+            style={styles.item}
+          >
+            <Image source={item.icon}/>
+            <Text style={styles.text}>{item.name + ": " + item.amount}</Text>
           </View>
-        }
-        />
+        )}
         <Modal
           animationType='slide'
           transparent={false}
           visible={this.state.showSellModal}
           onRequestClose={() => {this.setState({showSellModal: false})}}
         >
-        <View style={styles.buttons3}>
-          <View style={styles.buttonContainer}>
-            <Button style={styles.button} title='Sell 1' onPress={() => {this.sellItem(this.state.modalItem, 1)}}/>
+        <View style={modal.buttons3}>
+          <View style={modal.buttonContainer}>
+            <Button style={modal.button} title='Sell 1' onPress={() => {this.sellItem(this.state.modalItem, 1)}}/>
         </View>
-          <View style={styles.buttonContainer}>
-            <Button style={styles.button} title='Sell 10' onPress={() => {this.sellItem(this.state.modalItem, 10)}}/>
+          <View style={modal.buttonContainer}>
+            <Button style={modal.button} title='Sell 10' onPress={() => {this.sellItem(this.state.modalItem, 10)}}/>
         </View>
-          <View style={styles.buttonContainer}>
-            <Button style={styles.button} title='Sell All' onPress={() => {this.sellItem(this.state.modalItem, this.state.modalItem.amount)}}/>
+          <View style={modal.buttonContainer}>
+            <Button style={modal.button} title='Sell All' onPress={() => {this.sellItem(this.state.modalItem, this.state.modalItem.amount)}}/>
         </View>
         </View>
         </Modal>
@@ -67,27 +64,47 @@ export default class Shop extends React.Component {
   }
 }
 const styles = StyleSheet.create({
-  container: {
-   padding: 8,
-   width: '100%',
-   backgroundColor: 'green',
-  },
-  buttons3: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    width: '100%',
-    padding: 10,
-  },
-  button: {
-    padding: 16,
-    width: '100%',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 50,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    item: {
+      flex: 1,
+      height: 40,
+      padding: 4,
+      margin: 4,
+      backgroundColor: '#d68807',
+      flexDirection: 'row',
+      width: '50%',
+      maxWidth: '50%',
+      alignItems: 'center',
+    },
+    text: {
+      paddingLeft: 10,
+      fontSize: 18,
+    },
+    centerText: {
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontSize: 18,
+    }
+})
+
+const modal = StyleSheet.create({
+    buttons3: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        width: '100%',
+        padding: 10,
+    },
+    button: {
+        padding: 16,
+        width: '100%',
+    },
 })

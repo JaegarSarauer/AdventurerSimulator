1f71c68437888d676a5d0b7e1f0230d1052f74b5
Jaegar Sarauer <a00925935>
2018-04-24 00:17:58 -0700

    Viewable skills. Added with previous commit fot: State saving and loading every 30 seconds. Data storage for skills, inventory and bank. Skills and items defintions for basic few items and first skill. Player state class for handling user data, with data accessing and setting methods.

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, YellowBox } from 'react-native';
 import Home from './src/ui/Home';
 import Inventory from './src/ui/Inventory';
+import Skills from './src/ui/Skills';
 import { StackNavigator } from 'react-navigation';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 export default App = StackNavigator({
     Home: { screen: Home },
     Inventory: { screen: Inventory },
+    Skills: { screen: Skills },
 }, { headerMode: 'screen' });
